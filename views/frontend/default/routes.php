4bfda27e0ecdb6331dc96c4c635d62ee465f4abb
Orhan POLAT <opolat@hotmail.com>
2016-07-18 14:55:34 +0300

    Changed default template

--- views/frontend/default/routes.php
+++ views/frontend/default/routes.php
@@ -1,5 +1,5 @@
 <?php
-defined("WEBIM") or die("Dosya yok");
+defined('WEBIM') or die('Dosya yok');
 
 /**
  * @author Orhan POLAT
@@ -7,19 +7,24 @@ defined("WEBIM") or die("Dosya yok");
 
 use \System\Email;
 use \Webim\App;
-use \Webim\Http\Session;
-use \Webim\Image\Captcha;
 use \Webim\Library\Carbon;
 use \Webim\Library\File;
 use \Webim\Library\Message;
 use \Webim\View\Manager as View;
 
 App::make()->group('(/:lang#[a-z]{2}#)?', function () {
+ $folder = basename(dirname(__FILE__));
+
  //Default path
- $path = File::path('views.frontend.default');
+ $path = File::path('views.frontend.' . $folder);
 
  //Set default path
  View::setPath($path);
+
+ //Publish date
+ $date = Carbon::createFromTimestamp(
+  strtotime(conf('system.publish_date'))
+ );
 
  //Common conf
  $data = array(
@@ -27,17 +32,16 @@ App::make()->group('(/:lang#[a-z]{2}#)?', function () {
   'title' => conf('frontend.' . lang() . '.title', 'Web-IM XI'),
   'description' => conf('frontend.' . lang() . '.description', 'Web Internet Manager'),
   'keywords' => conf('frontend.' . lang() . '.keywords'),
-  'breadcrumb' => array()
+  'breadcrumb' => array(),
+  'date' => $date->toATOMString()
  );
 
+ //404
+ $this->setNotFoundTemplate(function ($title, $body) use ($data) {
+  return View::create('home')->data($data)->render();
+ });
+
  $this->get('/', function() use ($data) {
-  //Publish date
-  $date = Carbon::createFromTimestamp(
-   strtotime(conf('system.publish_date'))
-  );
-
-  $data['date'] = $date->toATOMString();
-
   return View::create('home')->data($data)->render();
  });
 
@@ -46,50 +50,34 @@ App::make()->group('(/:lang#[a-z]{2}#)?', function () {
 
   $message = Message::result('Geçersiz doğrulama kodu!');
 
-  $code = strtolower(Session::current()->get('captcha'));
+  if (filter_var(input('email'), FILTER_VALIDATE_EMAIL) !== false) {
+   //File
+   $file = View::getPath()->folder('layouts.email')->file('subscribe.html');
 
-  Session::current()->delete('captcha');
+   //Create and send
+   $send = Email::create('Web-IM E-Posta [' . conf('system.name', 'Web-IM XI') . ']')
+    ->from(conf('email.from'), conf('email.from_name'))
+    ->to(conf('email.from'), conf('email.from_name'))
+    ->html($file, array(
+     'ip' => $this->request->getClientIp(),
+     'date' => Carbon::now()->toDayDateTimeString(),
+     'email' => input('email')
+    ))->send();
 
-  if ($code == strtolower(input('code'))) {
-   if (filter_var(input('email'), FILTER_VALIDATE_EMAIL) !== false) {
-    //Create and send
-    $send = Email::create('Web-IM E-Posta [' . conf('system.name', 'Web-IM XI') . ']')
-     ->from(conf('email.from'), conf('email.from_name'))
-     ->to(conf('email.from'), conf('email.from_name'))
-     ->html(null, array(
-      'ip' => $this->request->getClientIp(),
-      'date' => Carbon::now()->toDayDateTimeString(),
-      'email' => input('email')
-     ))->send();
+   if ($send) {
+    //Save into file
+    File::path('assets', 'notify.txt')->create()->write(date('Y-m-d H:i:s') . ' - ' . input('email') . PHP_EOL, true);
 
-    if ($send) {
-     //Save into file
-     File::path('assets', 'notify.txt')->create()->write(date('Y-m-d H:i:s') . ' - ' . input('email') . PHP_EOL, true);
-
-     $message->success = true;
-     $message->text = 'E-posta adresiniz iletildi, teşekkür ederiz...';
-    } else {
-     $message->text = 'Kaydınız alınamadı, lütfen tekrar deneyiniz!';
-    }
+    $message->success = true;
+    $message->text = lang('message.subscription_success', 'Kaydınız alındı, teşekkür ederiz...');
    } else {
-    $message->text = 'Geçersiz e-posta adresi!';
+    $message->text = lang('message.subscription_failed', 'Kaydınız alınamadı, lütfen tekrar deneyiniz!');
    }
+  } else {
+   $message->text = lang('message.invalid_email_address', 'Geçersiz e-posta adresi!');
   }
 
   return $message->forData();
  });
 
- $this->get('/captcha', function($lang = null) {
-  //Fonts
-  $fonts = File::in('views.frontend.default.layouts.fonts')->fileIn('*.ttf')->files();
-
-  //Create
-  $captcha = Captcha::create($fonts);
-
-  //Set to check
-  Session::current()->set('captcha', $captcha->getStr());
-
-  //Display
-  $captcha->simple()->fontSize(14)->size(70, 20)->bgColor(238, 238, 238)->strColor(46, 50, 143)->display();
- });
 });
