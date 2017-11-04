211b28bfb16dd05134d0a42accc6f67b60a55362
Orhan POLAT <opolat@hotmail.com>
2017-11-04 15:47:15 +0300

    Some updates

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ define('PUB_ROOT', __DIR__ . DS);
 define('SYS_ROOT', PUB_ROOT . '..' . DS . 'sys' . DS);
 define('INDEX', basename(__FILE__));
 define('EXT', strrchr(INDEX, '.'));
-define('START_TIME', microtime());
+define('START_TIME', microtime(true));
 
 if (!file_exists(SYS_ROOT)) {
   die('System folder not found!');
